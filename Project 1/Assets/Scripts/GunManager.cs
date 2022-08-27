2d2575a631af3ddfdb8e1ae5024e9cce19170bef
kingdomx01 <51502661+kingdomx01@users.noreply.github.com>
2022-08-27 12:18:30 +0700

    complete animation

--- Project 1/Assets/Scripts/GunManager.cs
+++ Project 1/Assets/Scripts/GunManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,13 +6,41 @@ using UnityEngine;
 public class GunManager : MonoBehaviour
 {
     [SerializeField] private ScriptableObjectGun data;
-    [SerializeField] private List<Transform> gun;
+    [SerializeField] private List<Transform> guns;
     [SerializeField] private Transform gunParent;
+    [SerializeField] private int levelGun;
     private void Start()
     {
+        levelGun = 0;
         for (int i=0;i < gunParent.childCount;i++)
         {
-            gun.Add(gunParent.GetChild(i).gameObject.transform);
+            guns.Add(gunParent.GetChild(i).gameObject.transform);
+        }
+        foreach (Transform gun in guns)
+        {
+            gun.gameObject.SetActive(false);
+        }
+        guns[levelGun].gameObject.SetActive(true);
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            levelGun++;
+            UpgradeGunLevel();
+        }
+    }
+
+    private void UpgradeGunLevel()
+    {
+        if (levelGun < guns.Count)
+        {
+            guns[levelGun - 1].gameObject.SetActive(false);
+            guns[levelGun].gameObject.SetActive(true);
+        }
+        else
+        {
+            levelGun = guns.Count;
         }
     }
 
